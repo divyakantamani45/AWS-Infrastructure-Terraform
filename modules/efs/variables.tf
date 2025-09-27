cf769ce5ae4a7b94ca965af390c66ec0e70d71d1
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-27 22:06:24 +0530

    Update variables.tf

--- modules/efs/variables.tf
+++ modules/efs/variables.tf
@@ -1,4 +1,7 @@
 variable "vpc_id" { type = string }
 variable "subnet_ids" { type = list(string) }
-variable "vpc_sg_id" { type = string, default = "" }
+variable "vpc_sg_id" {
+  type = string,
+  default = "" 
+}
 variable "region" { type = string }
