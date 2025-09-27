4782088837c024bcd37bdd47220e9bee22e937ec
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-27 19:28:35 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "devops-eks"
+}
+
+variable "domain_name" {
+  description = "Route53 domain used for the app (example: app.example.com)"
+  type        = string
+  default     = "app.devops.com"
+}
+
+variable "vpc_cidr" { type = string default = "10.0.0.0/16" }
