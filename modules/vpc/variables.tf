44637607215508108e9fb9846deea362a7493745
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-27 22:48:19 +0530

    update var file

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,7 +1,5 @@
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+   type = string 
+   default = "10.0.0.0/16" 
 }
-variable "region" {
-  type = string
-}
+variable "region" { type = string }
