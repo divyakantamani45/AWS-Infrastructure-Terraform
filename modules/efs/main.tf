3d010db79af707cba589a786c9dad99e3e62ae20
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-27 19:39:19 +0530

    Create main.tf

---
+++ modules/efs/main.tf
@@ -0,0 +1,32 @@
+resource "aws_efs_file_system" "this" {
+  creation_token = "${var.region}-efs-${random_id.id.hex}"
+  lifecycle_policy {
+    transition_to_ia = "AFTER_14_DAYS"
+  }
+}
+
+resource "random_id" "id" { byte_length = 4 }
+
+resource "aws_efs_mount_target" "this" {
+  for_each = toset(var.subnet_ids)
+  file_system_id = aws_efs_file_system.this.id
+  subnet_id = each.key
+  security_groups = [var.vpc_sg_id]
+}
+
+# Optional: Access Point for per-app mount
+resource "aws_efs_access_point" "app_ap" {
+  file_system_id = aws_efs_file_system.this.id
+  posix_user {
+    gid = 1000
+    uid = 1000
+  }
+  root_directory {
+    path = "/app"
+    creation_info {
+      owner_gid = 1000
+      owner_uid = 1000
+      permissions = "0755"
+    }
+  }
+}
