3960881d9076ac529050403a433136207da0e626
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-27 19:36:33 +0530

    Create main.tf

---
+++ modules/rds/main.tf
@@ -0,0 +1,17 @@
+resource "aws_db_subnet_group" "this" {
+  name       = "${var.db_name}-subnet-group"
+  subnet_ids = var.db_subnet_ids
+}
+
+resource "aws_db_instance" "this" {
+  identifier = "${var.db_name}-instance"
+  engine = "postgres"
+  engine_version = "15.5"
+  instance_class = "db.t3.medium"
+  username = var.db_username
+  password = var.db_password
+  allocated_storage = 20
+  skip_final_snapshot = true
+  db_subnet_group_name = aws_db_subnet_group.this.name
+  vpc_security_group_ids = var.vpc_security_group_ids
+}
