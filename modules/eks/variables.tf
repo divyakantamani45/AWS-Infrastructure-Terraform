91b6f8f766260c72aebc4ed9cef5840da639d58e
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-27 19:33:53 +0530

    Create variables.tf

---
+++ modules/eks/variables.tf
@@ -0,0 +1,5 @@
+variable "cluster_name" { type = string }
+variable "vpc_id" { type = string }
+variable "private_subnets" { type = list(string) }
+variable "public_subnets" { type = list(string) }
+variable "region" { type = string }
