25d1736cc700ee111dc794816521ded716c8499c
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-28 00:10:35 +0530

    update fixes

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -2,8 +2,8 @@ output "cluster_id" {
   value = module.eks_cluster.cluster_id
 }
 output "oidc_provider_url" {
-  value = module.eks_cluster.cluster_oidc_issuer_url
+  value = module.eks_cluster.oidc_issuer_url
 }
 output "oidc_provider_arn" {
-  value = module.eks_cluster.cluster_oidc_issuer_arn
+  value = module.eks_cluster.oidc_issuer_arn
 }
