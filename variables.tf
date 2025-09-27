20287b95e0b6ef4452561b672b13245790acae6f
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-27 22:58:35 +0530

    update var file

--- variables.tf
+++ variables.tf
@@ -15,4 +15,7 @@ variable "domain_name" {
   default     = "app.devops.com"
 }
 
-variable "vpc_cidr" { type = string default = "10.0.0.0/16" }
+variable "vpc_cidr" { 
+  type = string 
+  default = "10.0.0.0/16" 
+}
