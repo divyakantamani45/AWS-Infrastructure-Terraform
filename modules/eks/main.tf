33e2851d9d97ea309b7023861c3472d530ff018f
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-29 02:10:39 +0530

    update

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,20 +2,18 @@ module "eks_cluster" {
   source = "terraform-aws-modules/eks/aws"
   version = "21.3.1" # pin an appropriate version
 
-  eks_cluster = {
-    name    = var.cluster_name
-    version = "1.27"
-  }
+  cluster_name = var.cluster_name
+  cluster_version = "1.27"
   subnet_ids = var.private_subnets
   vpc_id = var.vpc_id
 
   eks_managed_node_groups = {
     default = {
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 1
-      instance_types = ["t3.small"]
-      capacity_type  = "SPOT"
+      desired_capacity = 1
+      max_capacity     = 1
+      min_capacity     = 1
+      instance_types   = ["t3.small"]
+      capacity_type    = "SPOT"
     }
   }
 }
