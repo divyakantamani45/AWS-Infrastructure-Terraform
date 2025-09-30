86cf0facef87ba7ee5468b1611bc2a94691b02dd
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-10-01 02:43:11 +0530

    update

--- main.tf
+++ main.tf
@@ -93,6 +93,7 @@ module "alb_controller" {
     helm        = helm
     kubernetes  = kubernetes
   }
+  count = module.eks_cluster.cluster_name != "" ? 1 : 0
   cluster_name = module.eks.cluster_name
   vpc_id = module.vpc.vpc_id
   region = var.region
