287b1926e7b8fcd35077fc8eef6cb17b16e844ff
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-09-30 14:17:43 +0530

    updtae

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,7 +8,11 @@ resource "aws_subnet" "private" {
   vpc_id = aws_vpc.this.id
   cidr_block = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  tags = { Name = "private-${count.index}" }
+  tags = {
+    Name                                      = "private-${count.index}"
+    "kubernetes.io/cluster/devops-eks-prod"   = "shared"
+    "kubernetes.io/role/internal-elb"         = "1"
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -17,7 +21,11 @@ resource "aws_subnet" "public" {
   cidr_block = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index + 10)
   availability_zone = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
-  tags = { Name = "public-${count.index}" }
+  tags = {
+    Name                                    = "public-${count.index}"
+    "kubernetes.io/cluster/devops-eks-prod" = "shared"
+    "kubernetes.io/role/elb"                = "1"
+  }
 }
 
 data "aws_availability_zones" "available" {}
