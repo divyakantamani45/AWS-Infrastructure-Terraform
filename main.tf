72439b1eb42a64d0d5f7f439ffd076370ca86650
divyakantamani45 <50201679+divyakantamani45@users.noreply.github.com>
2025-10-01 02:46:47 +0530

    update

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ module "alb_controller" {
     helm        = helm
     kubernetes  = kubernetes
   }
-  count = module.eks_cluster.cluster_name != "" ? 1 : 0
+  count = can(regex("localhost", data.aws_eks_cluster.cluster.endpoint)) ? 0 : 1
   cluster_name = module.eks.cluster_name
   vpc_id = module.vpc.vpc_id
   region = var.region
